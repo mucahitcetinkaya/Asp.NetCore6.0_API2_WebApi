15b097c2eae8d6dee6873d56173ee05fb04aac62
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2023-09-29 11:35:30 +0300

    modeller ile calısma

---
+++ 4.Modeller_ile_Calisma/4.3_ApplicationContext/bookDemo/bookDemo/Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using bookDemo.Models;
+
+namespace bookDemo.Data
+{
+    public static class ApplicationContext
+    {
+        public static List<Book> Books { get; set; }
+        static ApplicationContext()
+        {
+            Books = new List<Book>()
+            {
+                new Book(){Id = 1, Title = "Hacivat ve Karagöz", Price = 50},
+                new Book(){Id = 2, Title = "Mesnevi", Price = 100},
+                new Book(){Id = 1, Title = "Dede Korkut", Price = 150},
+            };
+        }
+    }
+}
