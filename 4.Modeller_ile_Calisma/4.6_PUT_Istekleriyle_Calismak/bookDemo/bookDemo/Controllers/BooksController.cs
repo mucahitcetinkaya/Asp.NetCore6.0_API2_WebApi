051d6d001b2e1035f918f9ebe8bff524050bab47
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2023-10-01 17:09:51 +0300

    4. bolum bitti

---
+++ 4.Modeller_ile_Calisma/4.6_PUT_Istekleriyle_Calismak/bookDemo/bookDemo/Controllers/BooksController.cs
@@ -0,0 +1,72 @@
+﻿using bookDemo.Data;
+using bookDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookDemo.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationContext.Books.ToList();
+            return Ok(books);
+        }
+
+        [HttpGet("{id:int}")]
+        //public IActionResult GetOneBook(int id)
+        public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+        {
+            var book = ApplicationContext
+                .Books
+                .Where(b => b.Id.Equals(id))
+                .SingleOrDefault();
+
+            if (book is null)
+
+                return NotFound(); // 404
+
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+                ApplicationContext.Books.Add(book);
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneBook([FromRoute (Name = "id")] int id, [FromBody] Book book)
+        {
+            // check book ?
+            var entity = ApplicationContext
+                .Books
+                .Find(b => b.Id.Equals(id));
+            if (entity is null)
+            {
+                return NotFound(); // 404
+            }
+
+            // check id 
+            if (id != book.Id)
+                return BadRequest(); // 400
+
+            ApplicationContext.Books.Remove(entity);
+            book.Id = entity.Id;
+            ApplicationContext.Books.Add(book);
+            return Ok(book);
+        }
+    }
+}
